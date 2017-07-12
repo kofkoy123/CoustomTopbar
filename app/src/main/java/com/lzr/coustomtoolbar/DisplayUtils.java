dec31ec0ced7b0ac0bd16634b6d772fa5bf72073
kofkoy123 <125032854@qq.com>
2017-07-12 20:54:50 +0800

    自定义标题

---
+++ app/src/main/java/com/lzr/coustomtoolbar/DisplayUtils.java
@@ -0,0 +1,50 @@
+package com.lzr.coustomtoolbar;
+
+import android.content.Context;
+import android.util.DisplayMetrics;
+import android.view.WindowManager;
+
+/**
+ * Created by Administrator on 2017/7/12.
+ * 显示工具类 尺寸转换 屏幕宽高等
+ */
+
+public class DisplayUtils {
+
+    //根据手机的分辨率从dp转成为px
+    public static int dip2px(Context context, float dpValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+    //根据手机的分辨率从px的单位转为dp
+    public static int px2dip(Context context, float pxValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
+
+    //获取屏幕宽
+    public static int getSreenWidth(Context context) {
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics dm = new DisplayMetrics();
+        wm.getDefaultDisplay().getMetrics(dm);
+        return dm.widthPixels;
+    }
+
+    //获取屏幕高度
+    public static int getSreenHeight(Context context) {
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics dm = new DisplayMetrics();
+        wm.getDefaultDisplay().getMetrics(dm);
+        return dm.heightPixels;
+    }
+
+    //获取像素密度
+    public static float getSreenDensity(Context context) {
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics dm = new DisplayMetrics();
+        wm.getDefaultDisplay().getMetrics(dm);
+        return dm.density;
+    }
+
+}
